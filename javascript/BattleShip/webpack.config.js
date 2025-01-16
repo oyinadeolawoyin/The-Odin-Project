2534e70280b1fb62c22ff19fd530b56d091826c9
oyinade olawoyin <oyinadeolawoyin@proton.me>
2025-01-16 18:05:46 +0100

    Add webpack to project

--- javascript/BattleShip/webpack.config.js
+++ javascript/BattleShip/webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: "./src/main.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
