6aaba7d851efce9242744a1e79dcf7b95c6dfb25
oyinade olawoyin <oyinadeolawoyin@proton.me>
2025-04-26 02:20:57 +0100

    Edit code using express

--- Node.js/BasicInfo-site/demo_fileserver.js
+++ Node.js/BasicInfo-site/demo_fileserver.js
@@ -1,21 +1,13 @@
 const express = require("express");
-const fs = require("fs");
-const url = require("url");
+const path = require("path");
 
 const app = express();
 
-app.get("/", function (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = "." + q.pathname;
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 const PORT = 8080;
